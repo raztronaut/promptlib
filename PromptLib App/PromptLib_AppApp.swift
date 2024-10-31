47676cf3f10a2b4779c53f98f4ac0b633e0e3dab
Razi Syed <syed.raziulhaque@gmail.com>
2024-10-31 01:24:40 -0400

    Initial Commit

---
+++ PromptLib App/PromptLib_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptLib_AppApp.swift
+//  PromptLib App
+//
+//  Created by Razi Syed on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct PromptLib_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PromptLib AppTests/PromptLib_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  PromptLib_AppTests.swift
+//  PromptLib AppTests
+//
+//  Created by Razi Syed on 10/31/24.
+//
+
+import Testing
+
+struct PromptLib_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
